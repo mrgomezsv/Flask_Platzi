cbd9223aaf7aa5f960a1d98c80efdf34f0af5f88
Mario Roberto Gómez <mrgomez.dev@gmail.com>
2022-08-29 23:24:31 -0600

    Templates con Jinja 2

--- platzi-flask/main.py
+++ platzi-flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
@@ -15,7 +15,8 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
-    return 'Hello World Platzi, tu IP es {}'.format(user_ip)
+    #return 'Hello World Platzi, tu IP es {}'.format(user_ip)
+    return render_template('hello.html', user_ip=user_ip)
 
     #Siempre correr set FLASK_APP=main.py en la terminal para evitar el erro de varialbes
 # if __name__ == '__main__':
